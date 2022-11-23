eb1a44bf10f6b819f6bf382f436b5ac8b6d33fce
Khushwant Singh Rathore <51788179+khushwantrathore7865gmailcom@users.noreply.github.com>
2022-11-23 17:55:29 +0530

    Update settings.py

--- movie/movie/settings.py
+++ movie/movie/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^n5mkx#5!p5=b+dvori%=-qac-zuivc1x3ekl3fbmiafqv699b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [wolkusmovi.herokuapp.com]
 
 
 # Application definition
